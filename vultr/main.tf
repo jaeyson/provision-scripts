d55057b7c5de1a30dd1905d0df1de57a956d3414
Jaeyson Anthony Y. ⚗️ <hello@jaeyson.dev>
2025-05-19 00:25:47 +0800

    add packer (#1)

--- vultr/main.tf
+++ vultr/main.tf
@@ -10,13 +10,14 @@ provider "vultr" {
 }
 
 resource "vultr_instance" "web" {
-  count    = 1
-  plan     = var.vm_plan
-  region   = var.vm_region
-  image_id = var.image_id
-  label    = var.vm_label == null ? random_pet.instance[0].id : var.vm_label
-  hostname = var.vm_hostname
-  backups  = var.is_backups_enabled
+  count       = 1
+  plan        = var.vm_plan
+  region      = var.vm_region
+  image_id    = var.image_id
+  label       = var.vm_label == null ? random_pet.instance[0].id : var.vm_label
+  hostname    = var.vm_hostname
+  backups     = var.is_backups_enabled
+  snapshot_id = var.snapshot_id
   ssh_key_ids = [
     vultr_ssh_key.ssh_key.id
   ]
--- vultr/variables.tf
+++ vultr/variables.tf
@@ -7,23 +7,32 @@ variable "vultr_api_key" {
 variable "vm_plan" {
   type        = string
   description = "VM plan based on region-id"
-  default     = "vc2-1c-1gb"
+  default     = null
 }
 
 variable "vm_region" {
   type        = string
   description = "The ID of the region that the instance is to be created in."
-  default     = "sea"
+  default     = null
 }
 
 # variable "os_id" {
 #   type        = number
 #   description = "The ID of the operating system to be installed on the server"
+#   default     = null
 # }
+
+variable "snapshot_id" {
+  type        = string
+  description = "The ID of the Vultr snapshot that the server will restore for the initial installation."
+  default     = null
+  sensitive   = true
+}
 
 variable "image_id" {
   type        = any
   description = "The ID of the Vultr marketplace application to be installed on the server. Note marketplace applications are denoted by type: marketplace and you must use the image_id not the id."
+  default     = null
 }
 
 variable "is_backups_enabled" {
