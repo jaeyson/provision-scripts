bb1057963ddc8f4feaff68566461af157ebd26fe
jaeyson <hello@jaeyson.dev>
2025-07-12 23:05:50 +0800

    update web_app

--- vultr-actix-web/web_app/src/main.rs
+++ vultr-actix-web/web_app/src/main.rs
@@ -1,4 +1,10 @@
-use actix_web::{App, HttpServer, Responder, get};
+use actix_web::App;
+use actix_web::HttpServer;
+use actix_web::Responder;
+use actix_web::get;
+use openssl::ssl::SslAcceptor;
+use openssl::ssl::SslFiletype;
+use openssl::ssl::SslMethod;
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -7,20 +13,18 @@ async fn hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+    builder
+        .set_private_key_file(
+            "/etc/letsencrypt/live/test.nappy.co/privkey.pem",
+            SslFiletype::PEM,
+        )
+        .unwrap();
+    builder
+        .set_certificate_chain_file("/etc/letsencrypt/live/test.nappy.co/fullchain.pem")
+        .unwrap();
     HttpServer::new(|| App::new().service(hello))
-        .bind_openssl(
-            "0.0.0.0:443",
-            openssl::ssl::SslAcceptor::mozilla_intermediate(openssl::ssl::SslMethod::tls())
-                .unwrap()
-                .set_private_key_file(
-                    "/etc/letsencrypt/live/test.nappy.co/privkey.pem",
-                    openssl::ssl::SslFileType::PEM,
-                )
-                .set_certificate_chain_file(
-                    "/etc/letsencrypt/live/test.nappy.co/fullchain.pem",
-                    openssl::ssl::SslFileType::PEM,
-                ),
-        )?
+        .bind_openssl("0.0.0.0:443", builder)?
         .run()
         .await
 }
